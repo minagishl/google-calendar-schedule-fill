a496a4e087f8c80e620e6a0c8b1740ea11d27ea4
minagishl <mvxd568.co@icloud.com>
2026-06-23 15:15:32 +0900

    chore: update to version 1.5.1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 const manifest = defineManifest({
   manifest_version: 3,
   name: "Google Calendar Schedule Fill",
-  version: "1.5.0",
+  version: "1.5.1",
   description:
     "Automatically fill your availability on scheduling sites from Google Calendar.",
   permissions: ["storage", "unlimitedStorage"],
